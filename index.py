5a0fa94383ef80dfbbb4e6e4ed95a51d1e32c85e
ljy <eejyng94@gmail.com>
2022-12-12 20:07:42 +0900

    commit

--- index.py
+++ index.py
@@ -123,5 +123,5 @@ def processing(sentence):
         return sentence
     except:
         pass
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000)
+#if __name__ == '__main__':
+#    app.run(host='127.0.0.1', port=3000)
